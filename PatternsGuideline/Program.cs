8761013fd42dcd70b2e142eab3658e0e9774a768
denispetr567 <denispetr567@gmail.com>
2021-11-15 15:42:05 +0200

    Добавьте файлы проекта.

---
+++ PatternsGuideline/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PatternsGuideline
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
